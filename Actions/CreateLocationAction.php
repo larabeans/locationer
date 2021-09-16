a14606592204401f3a24b4727aa13bad5ed02515
Sajjad Naqvi <sajjad@orenda.systems>
2021-09-16 17:11:05 +0500

    change location parameter

--- Actions/CreateLocationAction.php
+++ Actions/CreateLocationAction.php
@@ -13,8 +13,19 @@ use App\Ship\Parents\Requests\Request;
 class CreateLocationAction extends Action
 {
     public function run(CreateLocationRequest $data) : Location
-    {
-        $location = app(CreateLocationTask::class)->run($data);
+    {        
+        $location = app(CreateLocationTask::class)->run(
+        $data->locatable_type,
+        $data->locatable_id ,
+        $data->address_line_1 ,
+        $data->address_line_2 ,
+        $data->city_id ,
+        $data->state_id ,
+        $data->country_id ,
+        $data->post_code ,
+        $data->latitude ,
+        $data->longitude 
+    );
 
         return $location;
     }
