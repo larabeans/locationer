aef21875ac8b956c2af0c12918499e58abeedf7a
Sajjad Naqvi <sajjad@orenda.systems>
2021-09-21 17:18:32 +0500

    changes in response data

--- UI/API/Transformers/LocationTransformer.php
+++ UI/API/Transformers/LocationTransformer.php
@@ -39,9 +39,9 @@ class LocationTransformer extends Transformer
             'longitude' => $entity->longitude,
             'created_at' => $entity->created_at,
             'updated_at' => $entity->updated_at,
-            'country_name' =>$entity->country->name,
-            'state_name' =>$entity->state->name,
-            'city_name' =>$entity->city->name
+             'country_id' =>$entity->country->id,
+             'state_id' =>$entity->state->id,
+             'city_id' =>$entity->city->id
         ];
 
         $response = $this->ifAdmin([
