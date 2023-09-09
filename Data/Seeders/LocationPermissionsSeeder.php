57e19da24d4e6ccb6d6da9bad8d3839b8202a84a
Syed Ali Murtaza <ali@kazmi.me>
2023-09-09 11:50:13 +0500

    Fixes permission seeder

--- Data/Seeders/LocationPermissionsSeeder.php
+++ Data/Seeders/LocationPermissionsSeeder.php
@@ -9,30 +9,33 @@ class LocationPermissionsSeeder extends Seeder
 {
     public function run()
     {
-        // Default Location Management Permissions ----------------------------------------------------------
-        app(CreatePermissionTask::class)->run('manage-locations', 'Manage Location Permissions.');
-        app(CreatePermissionTask::class)->run('list-locations', 'View All Locations Permissions.');
-        app(CreatePermissionTask::class)->run('view-location', 'View Location Permissions.');
-        app(CreatePermissionTask::class)->run('update-location', 'Edit Location Permission.');
-        app(CreatePermissionTask::class)->run('delete-location', 'Delete Location Permissions.');
-        app(CreatePermissionTask::class)->run('create-location', 'Create Location Permission.');
+        foreach (array_keys(config('auth.guards')) as $guardName) {
+            // Default Location Management Permissions ----------------------------------------------------------
+            app(CreatePermissionTask::class)->run('manage-locations', 'Manage Location Permissions.', guardName: $guardName);
+            app(CreatePermissionTask::class)->run('list-locations', 'View All Locations Permissions.', guardName: $guardName);
+            app(CreatePermissionTask::class)->run('view-location', 'View Location Permissions.', guardName: $guardName);
+            app(CreatePermissionTask::class)->run('update-location', 'Edit Location Permission.', guardName: $guardName);
+            app(CreatePermissionTask::class)->run('delete-location', 'Delete Location Permissions.', guardName: $guardName);
+            app(CreatePermissionTask::class)->run('create-location', 'Create Location Permission.', guardName: $guardName);
 
-        app(CreatePermissionTask::class)->run('manage-countries', 'Manage Country Permissions.');
-        app(CreatePermissionTask::class)->run('view-country', 'View Country Permissions.');
-        app(CreatePermissionTask::class)->run('update-country', 'Edit Country Permission.');
-        app(CreatePermissionTask::class)->run('delete-country', 'Delete Country Permissions.');
-        app(CreatePermissionTask::class)->run('create-country', 'Create Country Permission.');
+            app(CreatePermissionTask::class)->run('manage-countries', 'Manage Country Permissions.', guardName: $guardName);
+            app(CreatePermissionTask::class)->run('view-country', 'View Country Permissions.', guardName: $guardName);
+            app(CreatePermissionTask::class)->run('update-country', 'Edit Country Permission.', guardName: $guardName);
+            app(CreatePermissionTask::class)->run('delete-country', 'Delete Country Permissions.', guardName: $guardName);
+            app(CreatePermissionTask::class)->run('create-country', 'Create Country Permission.', guardName: $guardName);
 
-        app(CreatePermissionTask::class)->run('manage-states', 'Manage States Permissions.');
-        app(CreatePermissionTask::class)->run('view-state', 'View State Permissions.');
-        app(CreatePermissionTask::class)->run('update-state', 'Edit State Permission.');
-        app(CreatePermissionTask::class)->run('delete-state', 'Delete State Permissions.');
-        app(CreatePermissionTask::class)->run('create-state', 'Create State Permission.');
+            app(CreatePermissionTask::class)->run('manage-states', 'Manage States Permissions.', guardName: $guardName);
+            app(CreatePermissionTask::class)->run('view-state', 'View State Permissions.', guardName: $guardName);
+            app(CreatePermissionTask::class)->run('update-state', 'Edit State Permission.', guardName: $guardName);
+            app(CreatePermissionTask::class)->run('delete-state', 'Delete State Permissions.', guardName: $guardName);
+            app(CreatePermissionTask::class)->run('create-state', 'Create State Permission.', guardName: $guardName);
 
-        app(CreatePermissionTask::class)->run('manage-cities', 'Manage Cities Permissions.');
-        app(CreatePermissionTask::class)->run('view-city', 'View City Permissions.');
-        app(CreatePermissionTask::class)->run('update-city', 'Edit City Permission.');
-        app(CreatePermissionTask::class)->run('delete-city', 'Delete City Permissions.');
-        app(CreatePermissionTask::class)->run('create-city', 'Create City Permission.');
+            app(CreatePermissionTask::class)->run('manage-cities', 'Manage Cities Permissions.', guardName: $guardName);
+            app(CreatePermissionTask::class)->run('view-city', 'View City Permissions.', guardName: $guardName);
+            app(CreatePermissionTask::class)->run('update-city', 'Edit City Permission.', guardName: $guardName);
+            app(CreatePermissionTask::class)->run('delete-city', 'Delete City Permissions.', guardName: $guardName);
+            app(CreatePermissionTask::class)->run('create-city', 'Create City Permission.', guardName: $guardName);
+        }
+        
     }
 }
